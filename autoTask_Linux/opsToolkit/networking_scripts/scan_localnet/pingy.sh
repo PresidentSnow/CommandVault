d7917efe5c4dd76216615a38a8edf792296b22dc
Danton <xaviyu2005@gmail.com>
2026-01-05 18:07:39 +0100

    Scan localnet script.
    - Networking script for scan specific IPv4 given in the file 'octets.txt'.
    - After the scan, the script execute 'nmap' command.

---
+++ autoTask_Linux/opsToolkit/networking_scripts/scan_localnet/pingy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+trap 'echo -e "\n ERROR!!!!: Check the steps for more details." >&2' ERR
+
+if [ "$EUID" -ne 0 ]; then
+    echo "You must be root to do this."
+    exit 1
+fi
+
+# Obtain the octets of the IPv4 address
+ifconfig | grep "broadcast" | cut -d " " -f 10 | grep "^192\.168\." | cut -d "." -f 1,2,3 | uniq > octets.txt
+
+OCTETS=$(cat octets.txt)
+echo " " > $OCTETS.txt
+
+# Loop
+for ip in {1..254}; do
+  ping -c 1 $OCTETS.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" >> $OCTETS.txt &
+done
+
+# Show octets file
+cat $OCTETS.txt | sort -nr > sorted_$OCTETS.txt
+echo " "
+
+# Scan
+nmap -sS --top-ports 20 -iL sorted_$OCTETS.txt
+
+exit

b0d92910e36539e993bd5145318cb1c55e16e912
eobardthawne <xaviyu2005@gmail.com>
2026-03-09 22:01:58 +0100

    feat: two new docker scripts for Fedora added.
    - Subject: Fedora scripts.
    - Topic: docker scripts.
    - Branch: fedoraScripts
    - Date: 2026/03/09
    - Changes: two new docker scripts for Fedora added, one for installation and the other one for uninstallation.

---
+++ autoTask_Linux/FedoraDistributions/Docker_Scripts/docker-uninstall.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Docker and docker compose uninstallation
+# automation on Fedora and dnf-based systems
+
+set -e
+trap 'echo -e "\n ERROR!!!!: Check the steps for more details." >&2' ERR
+
+clear_terminal(){
+    echo -n "The terminal will be cleared in: "
+    for i in {1..10}; do
+        echo -n "${i} "
+        sleep 2
+    done
+    echo ""
+    clear
+}
+
+osSystem=$(uname -s)
+
+if [[ "$osSystem" != "Linux" ]]; then
+    echo -e "ERROR: you are NOT in a Linux system.\n"
+    exit 1
+fi
+
+if [ "$EUID" -ne 0 ]; then
+    echo "You must be root to do this."
+    exit 1
+fi
+
+sudo dnf remove docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras
+
+sudo rm -rf /var/lib/docker
+sudo rm -rf /var/lib/containerd

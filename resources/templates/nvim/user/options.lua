82e823a2323af1d90cff59562ba8f4d55275946b
EobardThawne <xaviyu2005@gmail.com>
2026-03-22 21:18:29 +0100

    refactor and renamed: neovim configuration files and folders were reorganized.
    - Subject: templates.
    - Topic: neovim configuration.
    - Branch: resources
    - Date: 2026/03/22
    - Changes: the 'ui.lua' file was renamed to 'statusline.lua'. In addition, the 'lua/user/' folder structure has been removed and is now 'user/'.

--- resources/templates/nvim/user/options.lua
+++ resources/templates/nvim/user/options.lua
@@ -9,6 +9,7 @@ opt.relativenumber = false
 
 -- Indentation
 opt.tabstop = 4
+opt.softtabstop = 4
 opt.shiftwidth = 4
 opt.expandtab = true
 opt.smartindent = true
@@ -29,6 +30,7 @@ opt.showmatch = true
 -- General
 opt.mouse = ""
 opt.clipboard = "unnamedplus"
+opt.completeopt = {'menu', 'menuone', 'noselect'}
 opt.confirm = true
 opt.spell = true
 opt.history = 20

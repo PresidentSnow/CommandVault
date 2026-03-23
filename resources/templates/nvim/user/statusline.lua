aa40b6d0ed596777e85c5c113c018ce09b64f529
EobardThawne <xaviyu2005@gmail.com>
2026-03-24 00:03:53 +0100

    feat: new neovim configuration have been added.
    - Subject: templates.
    - Topic: neovim configuration files.
    - Branch: templates
    - Date: 2026/03/24
    - Changes: new features have been added in the neovim 'statusline' and show git status branch (not implemented).

--- resources/templates/nvim/user/statusline.lua
+++ resources/templates/nvim/user/statusline.lua
@@ -1,15 +1,17 @@
 -- Configuration of shortcuts
 local opt = vim.opt
 
--- Statusline (left side)
+-- clear the previous 'statusline' conf
 opt.statusline = ""
-opt.statusline:append("%F %M %Y %R")
-opt.statusline:append("%{strftime('%H:%M')}")
+opt.laststatus = 2
+
+-- Statusline (left side)
+opt.statusline:append("%f %M %Y %R")
+--opt.statusline:append("%{strftime('%H:%M')}")
 opt.statusline:append("[%{&fileencoding}:%{&fileformat}]")
-opt.statusline:append("mode: %{mode()}")
+--opt.statusline:append("mode: %{mode()}")
+opt.statusline:append("%p%%")
 
 -- Statusline (right side)
 opt.statusline:append("%=")
-opt.statusline:append("ascii: %b hex: 0x%B totalRow: %L row: %l col: %c percent: %p%%")
-opt.laststatus = 2
-
+opt.statusline:append("ascii: %b hex: 0x%B ln: %l/%L col: %c")
